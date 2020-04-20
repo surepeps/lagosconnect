bf221beac9aad9651c44ad1f81efd04bae2e723c
surepeps <gatukurh1@gmail.com>
2020-04-20 15:34:07 +0100

    Updates

---
+++ precache-manifest.601624ffc907bd2bd7170e4ac97dff67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97cf3db91ccf470fe848c7418a715de",
+    "url": "/lagosconnect/index.html"
+  },
+  {
+    "revision": "ab80ed3a2335050169fe",
+    "url": "/lagosconnect/static/js/2.475ee205.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lagosconnect/static/js/2.475ee205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8f5a6e005c540d5c79",
+    "url": "/lagosconnect/static/js/main.e1d15a2c.chunk.js"
+  },
+  {
+    "revision": "1dbe79c214ecbf8a3572",
+    "url": "/lagosconnect/static/js/runtime-main.5f7f1d53.js"
+  }
+]);
